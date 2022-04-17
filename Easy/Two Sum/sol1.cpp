41cabacc0c3dfc18e691c41c7563933a2189a982
curry0622 <kingkazma0112305@gmail.com>
2022-04-17 21:18:39 +0800

    feat(Easy/Two Sum): brute force solution

---
+++ Easy/Two Sum/sol1.cpp
@@ -0,0 +1,17 @@
+// brute force -> O(n^2)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums.size(); j++) {
+                if((nums.at(i) + nums.at(j) == target) && i != j) {
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+        return ans;
+    }
+};
