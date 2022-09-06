b94761a0a175de0b45e5ffae96395b2493ac6854
curry0622 <kingkazma0112305@gmail.com>
2022-09-06 23:19:21 +0800

    feat(Medium/62. Unique Paths): sol1

---
+++ Medium/62. Unique Paths/sol1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        m--;
+        n--;
+        return nCr(m + n, m);
+    }
+
+    int nCr(int n, int r) {
+        long long p = 1, k = 1;
+
+        // C(n, r) == C(n, n-r),
+        // choosing the smaller value
+        if (n - r < r)
+            r = n - r;
+
+        if (r != 0) {
+            while (r) {
+                p *= n;
+                k *= r;
+
+                // gcd of p, k
+                long long m = __gcd(p, k);
+
+                // dividing by gcd, to simplify
+                // product division by their gcd
+                // saves from the overflow
+                p /= m;
+                k /= m;
+
+                n--;
+                r--;
+            }
+
+            // k should be simplified to 1
+            // as C(n, r) is a natural number
+            // (denominator should be 1 ) .
+        }
+
+        return p;
+    }
+};
