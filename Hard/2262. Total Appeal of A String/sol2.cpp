daa5fcfc728c6fcb28ab8c6a8ef9f98cef219dac
curry0622 <kingkazma0112305@gmail.com>
2022-09-14 16:26:41 +0800

    feat(Hard/2262. Total Appeal of A String): sol2

---
+++ Hard/2262. Total Appeal of A String/sol2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+        long long ans = 0;
+        int n = s.size();
+        vector<vector<int>> lastIndex = vector<vector<int>>(26, vector<int>(n, -1));
+
+        for(int i = 0; i < 26; i++) {
+            for(int j = 0; j < n; j++) {
+                if(s[j] == char(i + 97)) {
+                    lastIndex[i][j] = j;
+                } else if(j > 0 && lastIndex[i][j - 1] != -1) {
+                    lastIndex[i][j] = lastIndex[i][j - 1];
+                }
+                ans += lastIndex[i][j] + 1;
+            }
+        }
+
+        return ans;
+    }
+};
