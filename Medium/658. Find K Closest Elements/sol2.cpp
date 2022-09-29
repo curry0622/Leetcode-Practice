42aed3b640e20eef0d257632f0df5f269e5689f2
curry0622 <kingkazma0112305@gmail.com>
2022-09-29 16:59:33 +0800

    feat(Medium/658. Find K Closest Elements): sol2

---
+++ Medium/658. Find K Closest Elements/sol2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int start = 0;
+        for(int i = k; i < arr.size(); i++) {
+            if(arr[i] == arr[start]) {
+                start++;
+            } else if(abs(x - arr[i]) < abs(x - arr[start])) {
+                start++;
+            } else {
+                break;
+            }
+        }
+        return vector<int>(arr.begin() + start, arr.begin() + start + k);
+    }
+};
