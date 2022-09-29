8978f0f7c4a2662101df9f0b394d961388b8059d
curry0622 <kingkazma0112305@gmail.com>
2022-09-29 21:38:57 +0800

    feat(Easy/606. Construct String from Binary Tree): sol3

---
+++ Easy/606. Construct String from Binary Tree/sol3.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    string tree2str(TreeNode* root) {
+        string ans = "";
+        traverse(root, ans);
+        return ans;
+    }
+
+    void traverse(TreeNode* root, string& ans) {
+        ans += to_string(root -> val);
+        if(root -> left) {
+            ans += '(';
+            traverse(root -> left, ans);
+            ans += ')';
+        }
+        if(root -> right) {
+            if(!root -> left)
+                ans += "()";
+            ans += '(';
+            traverse(root -> right, ans);
+            ans += ')';
+        }
+    }
+};
