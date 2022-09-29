f9198b50d8d9c595e4ff723f7407f7aebc9e9d4b
curry0622 <kingkazma0112305@gmail.com>
2022-09-29 20:51:43 +0800

    feat(Medium/814. Binary Tree Pruning): sol2

---
+++ Medium/814. Binary Tree Pruning/sol2.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if(root -> left)
+            root -> left = pruneTree(root -> left);
+        if(root -> right)
+            root -> right = pruneTree(root -> right);
+        return root -> val || root -> left || root -> right ? root : NULL;
+    }
+};
