9dd23d63d2179f89eaccae3ec19c028f62ed6870
curry0622 <kingkazma0112305@gmail.com>
2022-09-30 11:27:03 +0800

    feat(Medium/967. Numbers With Same Consecutive Differences): sol2

---
+++ Medium/967. Numbers With Same Consecutive Differences/sol2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> ans;
+        for(int i = 1; i < 10; i++)
+            genNum(n - 1, k, i, i, ans);
+        return ans;
+    }
+
+    void genNum(int n, int k, int sum, int last, vector<int>& ans) {
+        if(n == 0) {
+            ans.push_back(sum);
+            return;
+        }
+        for(int i = 0; i < 10; i++) {
+            if(abs(i - last) == k) {
+                genNum(n - 1, k, sum * 10 + i, i, ans);
+            }
+        }
+    }
+};
