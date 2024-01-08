3b07acfec0329099fb5399de8b4b39c7f49202b5
curry0622 <kingkazma0112305@gmail.com>
2024-01-08 12:00:20 +0800

    feat(Easy/938. Range Sum of BST): sol1, 2

---
+++ Easy/938. Range Sum of BST/sol2.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res;
+    Solution() {res = 0;}
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root) {
+            if (low <= root->val && root->val <= high) {
+                res += root->val;
+                rangeSumBST(root->left, low, high);
+                rangeSumBST(root->right, low, high);
+            } else if (root->val < low) {
+                rangeSumBST(root->right, low, high);
+            } else {
+                rangeSumBST(root->left, low, high);
+            }
+        }
+        return res;
+    }
+};
