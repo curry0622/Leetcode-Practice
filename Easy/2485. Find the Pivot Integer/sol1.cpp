de83a96110818cebdc8056219952832d2a5acf30
curry0622 <kingkazma0112305@gmail.com>
2024-03-13 14:57:25 +0800

    feat(Easy/2485. Find the Pivot Integer): sol1

---
+++ Easy/2485. Find the Pivot Integer/sol1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int total = n * (n + 1) / 2;
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (sum == total - sum - i)
+                return i;
+            sum += i;
+        }
+        return -1;
+    }
+};
