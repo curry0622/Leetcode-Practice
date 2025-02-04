bd5a6bb69176fc724ef230871df466d102b6b1c7
curry0622 <kingkazma0112305@gmail.com>
2025-02-04 19:08:17 +0800

    feat(Easy/1800. Maximum Ascending Subarray Sum): sol1

---
+++ Easy/1800. Maximum Ascending Subarray Sum/sol1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum = nums[0];
+        int res = sum;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > nums[i-1]) {
+                sum += nums[i];
+            } else {
+                sum = nums[i];
+            }
+            res = max(res, sum);
+        }
+        return res;
+    }
+};
